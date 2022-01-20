e57b0700c5f874d4d134fec8b26cee675b01e5ee
casisdead2772 <alex.kostykevich@gmail.com>
2022-01-20 17:22:53 +0300

    update composer depend

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace casisdead2772\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('postcode_checker');
+
+        return $treeBuilder;
+    }
+}
---
+++ src/PostcodeBundle/PostcodeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace casisdead2772\PostcodeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PostcodeBundle extends Bundle {
+
+}
